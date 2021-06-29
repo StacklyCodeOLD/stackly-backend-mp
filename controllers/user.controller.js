52b8d37204903dd89ad7564a10506d45c94ab3ad
Fernando Lopez <lopezfer@globalhitss.com>
2021-06-28 23:26:15 -0300

    fix: same origin

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -95,7 +95,7 @@ async function postLogin(req, res) {
                 return res
                   .status(200)
                   .cookie("jwt", `${jwt}`, {
-                    sameSite: "strict",
+                    sameSite: "None",
                     path: "/",
                     httpOnly: true,
                     secure: true,
