7cf265c73d2582142c4fbf149da7b9a88b56198a
Fernando Lopez <lopezfer@globalhitss.com>
2021-06-29 12:58:04 -0300

    fix: prueba sameSite

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -95,7 +95,7 @@ async function postLogin(req, res) {
                 return res
                   .status(200)
                   .cookie("jwt", `${jwt}`, {
-                    sameSite: "Lax",
+                    sameSite: true,
                     path: "/",
                     httpOnly: true,
                     secure: true,
